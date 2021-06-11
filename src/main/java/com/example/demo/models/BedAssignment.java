222fd677f559ca9b08703561fb8539ca1222d013
Cyebukayire <peacecyebukayire@gmail.com>
2021-06-11 11:39:26 +0200

    Bed Assignment

--- src/main/java/com/example/demo/models/BedAssignment.java
+++ src/main/java/com/example/demo/models/BedAssignment.java
@@ -1,4 +1,90 @@
 package com.example.demo.models;
 
-public class BedAssignment {
+import com.example.demo.enums.AssignmentStatus;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "course_assignment")
+public class BedAssignment implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    private String whenAssigned;
+    private String whenReleased;
+    private String assignedBy;
+    private String comments;
+    @Enumerated(EnumType.STRING)
+    private AssignmentStatus Assignmentstatus;
+    @ManyToOne
+    private Student student;
+    @ManyToOne
+    private Course bed;
+
+    public BedAssignment(){}
+
+    public BedAssignment(String assignedOn, String assignedBy, AssignmentStatus status,String lastStatusChangedOn){
+        this.assignedOn= LocalDateTime.now().toString();
+        this.assignedBy= assignedBy;
+        this.status= status;
+        this.lastStatusChangedOn= LocalDateTime.now().toString();
+    }
+
+    public CourseAssignment(Long id,String assignedOn, String assignedBy, AssignmentStatus status,String lastStatusChangedOn){
+        this.id = id;
+        this.assignedBy= assignedBy;
+        this.status= status;
+        this.lastStatusChangedOn= LocalDateTime.now().toString();
+    }
+
+    public String getAssignedBy() {
+        return assignedBy;
+    }
+
+    public void setAssignedBy(String assignedBy) {
+        this.assignedBy = assignedBy;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public AssignmentStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(AssignmentStatus status) {
+        this.status = status;
+    }
+
+    public String getLastStatusChangedOn() {
+        return lastStatusChangedOn;
+    }
+
+    public void setLastStatusChangedOn(String lastStatusChangedOn) {
+        this.lastStatusChangedOn = lastStatusChangedOn;
+    }
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
 }
